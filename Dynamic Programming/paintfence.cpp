de66d99e02f1fa36dfeade30650e68def853ab1c
Bhavin <Bhavinondhiya0@gmail.com>
2024-05-30 19:51:26 +0530

    Solved Paint Fence Using Dp-Tabulation

--- Dynamic Programming/paintfence.cpp
+++ Dynamic Programming/paintfence.cpp
@@ -26,6 +26,17 @@ int paintFence(int n, int k, vector<int>& dp){
     dp[n] = paintFence(n-2,k,dp)*(k-1) + paintFence(n-1,k,dp)*(k-1);
     return dp[n];
 }
+
+//solve using tabulation
+int paintFence(int n, int k){
+    vector<int> dp(n+1,0);
+    dp[1] = k;
+    dp[2] = k+k*(k-1);
+    for(int i = 3; i <= n; i++){
+        dp[i] = dp[i-2]*(k-1) + dp[i-1]*(k-1);
+    }
+    return dp[n];
+}
    
 
 
